3ce3c48d9648e50dcb637324df966ebdf56305b9
Taui Silva Lima <tauisilva@gmail.com>
2025-03-03 18:47:58 -0300

    Feat: Init project

---
+++ src/main/java/com/swagger_dark/swagger_dark/SwaggerDarkApplication.java
@@ -0,0 +1,13 @@
+package com.swagger_dark.swagger_dark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swagger_dark/swagger_dark/SwaggerDarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swagger_dark.swagger_dark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerDarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
